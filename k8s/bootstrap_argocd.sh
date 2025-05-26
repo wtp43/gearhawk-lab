945d663543c29708bee6a126ad23d79b2800bff6
William Tang <wjt40398@gmail.com>
2025-05-26 13:41:57 -0400

    update scripts, remove tcp gateway

--- k8s/bootstrap_argocd.sh
+++ k8s/bootstrap_argocd.sh
@@ -1,16 +1,6 @@
 #!/bin/zsh
 
-
-kubectl kustomize --enable-helm infra/controllers/cert-manager | kubectl apply -f -
-# kubectl apply -k infra/network/gateway
 kustomize build --enable-helm infra/controllers/argocd | kubectl apply -f -
 kubectl apply -k infra
 kubectl apply -k sets
-# kubectl apply -k infra/network
-# kubectl apply -k infra/network/cloudflared
-# kubectl apply -k infra/controllers
 kubectl apply -k apps
-
-# kubectl apply -k apps/external
-# kubectl apply -k apps/external/proxmox
-# kubectl apply -k apps/typesense
