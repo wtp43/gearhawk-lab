69e708261b27054a3b30887442e7761b8c302ef7
William Tang <wjt40398@gmail.com>
2025-08-21 23:07:06 -0400

    enable rotate-server-certificates and deploy metrics server

--- k8s/bootstrap_argocd.sh
+++ k8s/bootstrap_argocd.sh
@@ -4,3 +4,7 @@ kustomize build --enable-helm infra/controllers/argocd | kubectl apply -f -
 kubectl apply -k infra
 kubectl apply -k sets
 kubectl apply -k apps
+
+# metrics server
+kubectl apply -f https://raw.githubusercontent.com/alex1989hu/kubelet-serving-cert-approver/main/deploy/standalone-install.yaml
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
